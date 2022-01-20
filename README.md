08eec185c65b138086f6ac51f01085d3da4d8e9a
Rui Figueira <ruimvfigueira@gmail.com>
2022-01-20 21:45:27 +0000

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # FritzingParts
-My custom Fritzing parts
+
+My custom Fritzing parts.
+I use these just for Breadboard views, so they are not fully functional/correct Fritzing parts.
+
+
+# SVG files
+
+To edit the SVG files, I use Inkscape (https://inkscape.org/).
+
+## Inkscape extension
+
+I had some troubles getting those showing up correctly on Fritzing, especially test.
+To solve that, I hacked a little Inkscape extension that converts everything to paths and saves as another file with the suffix `_fritzing`.
+The extension is in the folder `com.crazygaze.inkscape.fritzing_export`, and the entire folder itself should be copied to `C:\Users\<USER>\AppData\Roaming\inkscape\extensions\`
+
+
+# Random notes
+
+* If a custom part shows up as red in the breadboard, it probably means not all the connectors are mapped in the parts editor.
+
+
+
+
+
---
+++ com.crazygaze.inkscape.fritzing_export/fritzing_export.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Copyright (C) 2022  Rui Figueira
+# This extension makes it easier to get things such as text to show up properly in Fritzing,
+# by converting all objects to paths and saving as <DocumentName>_fritzing.svg
+#
+"""
+This extension will pre-process a vector image by applying the operations:
+'EditSelectAllInAllLayers' and 'ObjectToPath'
+before calling the dialog File->Save As....
+"""
+
+import inkex
+import os
+from inkex.base import TempDirMixin
+from inkex.command import inkscape_command
+from inkex import load_svg
+from inkex.command import write_svg
+
+class FritzingExport(TempDirMixin, inkex.EffectExtension):
+    def effect(self):
+        tempDocument = load_svg(inkscape_command(
+            self.svg,
+            verbs=['EditSelectAllInAllLayers', 'EditUnlinkClone', 'ObjectToPath'],
+        ))
+
+        tempDocumentPath = os.path.splitext(self.document_path())[0] + "_fritzing.svg"
+        os.remove(tempDocumentPath)
+        write_svg(tempDocument, tempDocumentPath)
+
+if __name__ == '__main__':
+    FritzingExport().run()
+
